670b3719a9a8df1d82ea9ce17114cb18b7edd994
Tshepang Masia‎ <100064063+TSH3PH4NG@users.noreply.github.com>
2025-12-09 11:07:21 +0200

    Update song.js
    New and improved

--- plugins/song.js
+++ plugins/song.js
@@ -1,20 +1,78 @@
 const { command , ytdl , getBuffer , metaData }  = require("../lib");
-const yts = require("yt-search");
+const axios = require("axios");
 
 command({
-pattern: "song",
-fromMe: true
+pattern: "yx",
+fromMe: false,
+desc: "Download songs",
+type: "download"
 },
 async(message,match, m)=>{
 if(!match) return message.reply("_i need a query_");
 try{
   
-let { title , url } = await (await yts(match)).all[0]
-let  buff  = await ytdl(url);
-if(!Buffer.isBuffer(buff)) return; /*idk what to log so this will do for now*/
-buff = await metaData(title, buff);
+
+let { url } = await (await axios(`https://api-ij32.onrender.com/yts?q=${match}`)).data[0];
+let  final  = await ytdl(url);
+
+return await message.client.sendMessage(message.jid, { audio: { url: final }, mimetype: "audio/mpeg" }, { quoted: m });
+
+}catch(e){
+  await console.log(e);
+ }
+ 
+});
+
+command({
+pattern: "xy",
+fromMe: false,
+desc: "Download songs",
+type: "download"
+},
+async(message,match, m)=>{
+if(!match) return message.reply("_i need a query_");
+try{
   
-return await message.client.sendMessage(message.jid, { audio: buff , mimetype: "audio/mpeg" }, {quoted: m });
+let { title , url } = await ( await axios(`https://api-ij32.onrender.com/yts?q=${match}`)).data[0];
+let  doc_url  = await ytdl(url);
+
+/*if(!Buffer.isBuffer(buff)) return;
+buff = await metaData(title, buff);*/
+
+return await message.client.sendMessage(message.jid, { document: { url: doc_url }, mimetype: "audio/mpeg" , fileName: `${title}.mp3`}, { quoted: m });
+}catch(e){
+  await console.log(e);
+ }
+})
+
+command({
+pattern: "song",
+fromMe: false,
+desc: "Download songs",
+type: "download"
+},
+async(message,match, m)=>{
+if(!match) return message.reply("_i need a query_");
+try{
+  
+let { title , url , thumbnail } = await ( await axios(`https://api-ij32.onrender.com/yts?q=${match}`)).data[0];
+
+await message.client.sendButton(message, { image: { url: thumbnail } , caption: `Song: ${title}`,
+footer: "Made with ðŸ«¶ by Tshepang",
+buttons: [{
+	text: "audio",
+	id: `.yx  ${url}`,
+},{
+	text: "document",
+	id: `.xy  ${url}`,
+},{
+	options: {
+		text: "copy_url",
+		copy_code: url,
+	}
+}]
+}, { quoted: m })
+
 }catch(e){
   await console.log(e);
  }
